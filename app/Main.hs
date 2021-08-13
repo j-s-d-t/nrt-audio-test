55dd7e28501a9d555be3637e86f4bf7f9080b89b
Joseph <studio@josephsalmon.co.nz>
2021-08-13 14:12:18 +1200

    Added convolution
    Signed-off-by: Joseph <studio@josephsalmon.co.nz>

--- app/Main.hs
+++ app/Main.hs
@@ -1,56 +1,60 @@
-import           Codec.Wav                          (exportFile, importFile)
-import           DSP.Convolution                    (conv, test)
-import           Data.Array                         as Array ()
-import           Data.Array.Unboxed                 as UArray (elems, listArray)
-import           Data.Audio                         (Audio (Audio))
-import           Data.Int                           (Int32)
-import           Data.Maybe                         (fromMaybe)
-import           Numeric.Transform.Fourier.FFTUtils (write_rfft_info)
-import           System.IO                          (FilePath)
+import           Codec.Wav          (exportFile, importFile)
+import           DSP.Convolution    (conv)
+import           Data.Array         as Array (Array, elems, listArray)
+import           Data.Array.Unboxed as UArray (elems, listArray)
+import           Data.Audio         (Audio (Audio))
+import           Data.Int           (Int32)
+import           Data.Maybe         (fromMaybe)
+import           System.IO          (FilePath)
 
+
+
+maxInt32 :: Float
+maxInt32 = fromIntegral (maxBound::Int32)
+
+rate :: Int
+rate = 44100
+
+outLength = 1000000
 
 -- File input
-filename = "sample.wav"
+filename :: FilePath
+filename = "sounds/consynth.wav"
 
-inMain :: FilePath -> IO ()
-inMain path = do
+ir :: FilePath
+ir = "sounds/cave_ir.wav"
+
+fromSF :: FilePath -> IO [Float]
+fromSF path = do
   maybeAudio <- importFile path
   case maybeAudio :: Either String (Audio Int32) of
-    Left s -> putStrLn $ "wav decoding error: " ++ s
-    Right (Audio rate channels samples) -> do
-      putStrLn $ "rate = " ++ show rate
-      putStrLn $ "channels: " ++ show channels
-      print $ UArray.elems samples
+    Left s -> return []
+    Right (Audio rate channels samples) ->
+      return $ map (\sample -> fromIntegral sample / maxInt32) $ UArray.elems samples
+
 
 -- DSP processing happens here
 
-sinewave :: [Float]
-sinewave = map (\x -> sin ((fromInteger x) / 10.0)) [1..10000]
-
+convolve samps ir =
+    let sampArray ss = Array.listArray ((0, outLength)) ss
+    in
+      conv (sampArray samps) (sampArray ir)
 
 -- File output
 
-outMain :: FilePath -> IO ()
-outMain path = do
-  let fs :: [Float]
-      fs = sinewave
-      l = 1000
-      rate = 44100
-  -- Float is 32bit, so i use Int32 for each sample in the output.
-  let maxInt32 :: Float
-      maxInt32 = fromIntegral (maxBound::Int32)
-      -- actual transformation function
-      rounder :: Float -> Int32
+outMain :: FilePath -> [Float] -> IO ()
+outMain path samps = do
+  let rounder :: Float -> Int32
       rounder = round . (*maxInt32)
   exportFile path ( Audio rate 1 -- 1 channel
-                  $ UArray.listArray (0,l)
+                  $ UArray.listArray (0, outLength)
                   $ map rounder
-                  $ fs)
+                  $ samps)
 
 -- IO
 
 main = do
-  putStrLn $ "* Outputting the sound to "++filename
-  outMain filename
-  putStrLn $ "* Printing the content of "++filename
-  inMain filename
+  samples <- fromSF filename
+  ir <- fromSF ir
+  let echo = convolve samples ir
+  outMain "output/out.wav" $ Array.elems echo
